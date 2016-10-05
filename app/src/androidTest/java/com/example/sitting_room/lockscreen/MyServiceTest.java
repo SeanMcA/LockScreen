5fdd5e3286241888ae6ef5a9e869320390ff3e96
Sean McAteer <seanmcateer100@hotmail.com>
2016-10-05 19:43:26 +0100

    After MyReceiver setup

---
+++ app/src/androidTest/java/com/example/sitting_room/lockscreen/MyServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.sitting_room.lockscreen;
+
+import junit.framework.TestCase;
+
+import dalvik.annotation.TestTarget;
+
+/**
+ * Created by sitting-room on 03/10/2016.
+ */
+public class MyServiceTest extends TestCase {
+
+    public void testOnHandleIntent() throws Exception {
+
+
+    }
+}
